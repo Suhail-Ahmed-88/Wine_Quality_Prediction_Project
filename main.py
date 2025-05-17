b04460f0712048bcc3db0669096aaeff6af2a2fc
Suhail-Ahmed-88 <rajpar.suhail.ahmed@gmail.com>
2025-05-17 16:50:33 +0800

    logger, exeception and utils added

---
+++ main.py
@@ -0,0 +1,5 @@
+# from mlproject import logger
+from mlproject.logger import logger
+
+logger.info("Welcome to our custom logging")
+logger.info("Change logging file")
--- setup.py
+++ setup.py
@@ -1,8 +1,6 @@
 from setuptools import find_packages, setup
 from typing import List
 
-from typing import List
-
 def get_requirements() -> List[str]:
     requirements_list: List[str] = []
     return requirements_list
